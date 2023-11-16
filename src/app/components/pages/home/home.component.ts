0340be250198b803e5dfac3bf8a74313e2f32e1a
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-11-16 21:35:48 +0100

    16-11

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -13,6 +13,7 @@ export class HomeComponent implements OnInit {
 
   foods:Food[] = [];
   constructor(private foodService:FoodService,activatedRoute:ActivatedRoute) {
+
     let foodsObservable:Observable<Food[]>;
     activatedRoute.params.subscribe((params)=>{
       if(params.searchTerm)
